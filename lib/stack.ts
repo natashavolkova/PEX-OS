0b13d0fc6168f6b360ee2022bfd13108174cb0d4
natashavolkova <natashavolkova771@gmail.com>
2025-12-07 12:06:14 -0300

    chore: remove unused @stackframe/stack - 0 vulnerabilities build

--- lib/stack.ts
+++
@@ -1,8 +0,0 @@
-import { StackClientApp } from '@stackframe/stack';
-
-const stack = new StackClientApp({
-    projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID!,
-    tokenStore: 'nextjs-cookie',
-});
-
-export default stack;
